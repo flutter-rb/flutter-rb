a365166c4ef68a8e874e33e6ea45e6a2e767bb9b
fartem <jaman.smlnsk@gmail.com>
2020-12-15 15:24:01 +0300

    2020-12-15 Version 0.4.2: Fixed tests

--- test/flutter-rb/project/test_project_parser.rb
+++ test/flutter-rb/project/test_project_parser.rb
@@ -4,7 +4,7 @@ require_relative '../../../lib/flutter-rb/project/project_parser.rb'
 
 class ProjectParserTest < Minitest::Test
   def test_project_not_exists
-    project = FlutterRb::ProjectParser.new('.')
+    project = FlutterRb::ProjectParser.new('.').project
 
     assert project.nil?
   end
