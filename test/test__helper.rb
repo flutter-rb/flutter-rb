74000f3793ee4330d7ebaf3da5d576dfcb35dcc5
fartem <jaman.smlnsk@gmail.com>
2021-01-18 20:09:06 +0300

    2021-01-18 Version 0.6.2: Fixed tests

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -3,8 +3,10 @@ SimpleCov.start do
   add_filter '/test/'
 end
 
-
-if ENV['CI']
-  require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
 end
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start
